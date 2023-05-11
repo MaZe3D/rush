2b5af3cf1ed2285dc351e15a835f542160e842e7
Benedikt Hauser <inf21037@lehre.dhbw-stuttgart.de>
2023-05-12 00:15:05 +0200

    add error handling and styling to tcp console

--- rush_client/src/main.rs
+++ rush_client/src/main.rs
@@ -1,15 +1,15 @@
-use std::error::Error;
-use std::io::Write;
-use std::vec;
+use core::panic;
+use std::error::{Error};
+use std::io::stdout;
 
 use async_std::net::{TcpStream, SocketAddr};
-use async_std::io::{prelude::*, stdin, BufReader};
+use async_std::io::{prelude::*};
 
 use clap::Parser;
-use crossterm::{cursor, ExecutableCommand,};
-use crossterm::event::{poll, read, Event, KeyCode, EventStream, KeyEvent, KeyModifiers, DisableFocusChange};
-use crossterm::style::{Print};
-use crossterm::terminal::{Clear, size};
+use crossterm::{cursor, ExecutableCommand, execute};
+use crossterm::event::{Event, KeyCode, EventStream, KeyEvent, KeyModifiers, DisableFocusChange};
+use crossterm::style::{Print, SetForegroundColor, Color, ResetColor};
+use crossterm::terminal::{Clear};
 
 use futures::{FutureExt, select, StreamExt};
 
@@ -19,38 +19,70 @@ struct Cli{
     listen_address: SocketAddr,
 }
 
+//move the cursor to the bottom line of the console, at the specified column
 fn move_cursor(n: u16) -> std::io::Result<()>{
-    use std::io::{stdout};
     let bottom_row = crossterm::terminal::size()?.0 - 1;
     stdout().execute(cursor::MoveTo(n, bottom_row))?;
     Ok(())
 }
 
+//print a vector of chars to the bottom line of the console
 fn write_vec_to_console(vec: &Vec<char>){
-    use std::io::{stdout};
-    stdout().execute(cursor::SavePosition);
-    stdout().execute(cursor::MoveToColumn(0));   
+        match execute!{
+        stdout(),
+        cursor::SavePosition,
+        cursor::MoveToColumn(0),
+        Clear(crossterm::terminal::ClearType::CurrentLine),
+    }{
+        Ok(_) => {}
+        Err(e) => println!("Error: {:?}", e),
+    };
     print!("{}", vec.iter().fold(String::new(), |acc, &num| acc + &num.to_string()));
-    stdout().execute(cursor::RestorePosition);
+    stdout().execute(cursor::RestorePosition);    
 }
 
-#[async_std::main]
-async fn main()-> Result<(), Box<dyn Error>> {
-    use std::io::stdout;
-    stdout()
-        .execute(cursor::EnableBlinking)?
-        .execute(DisableFocusChange)?
-        .execute(Clear(crossterm::terminal::ClearType::All))?;
+fn print_with_style(buffer: Vec<u8>, start_char: char, color: Color){
+    let _output_string = match String::from_utf8(buffer){
+        Ok(output_string) => {
+            match execute!{
+                stdout(),
+                cursor::MoveToColumn(0),
+                Clear(crossterm::terminal::ClearType::CurrentLine),
+                SetForegroundColor(color),
+                Print(start_char),
+                Print(output_string),
+                ResetColor,
+            }{
+                Ok(_) => {}
+                Err(e) => println!("Error: {:?}", e),
+            };
+            }
+        Err(e) => println!("Error: {:?}", e),
+    };
+}
 
-    let cli = Cli::parse();
-    println!("Listening on: {}", cli.listen_address);
+async fn connect_to_tcp(address:SocketAddr)->TcpStream{
+    while let mut try_stream = TcpStream::connect(address).await{
+        match try_stream{
+            Ok(try_stream) => {
+                println!("Connected to server");
+                return try_stream;
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+                println!("Retrying in 5 seconds");
+                async_std::task::sleep(std::time::Duration::from_secs(5)).await;
+            }
+        }
+    }
+    panic!("Could not connect to server");
+}
 
-    let mut input_string = String::new();
-    let mut stream = TcpStream::connect(&cli.listen_address).await?;
+async fn main_loop(address:SocketAddr)->Result<(), impl Error>{
+    let mut stream = connect_to_tcp(address).await;
     let mut buffer = [0u8; 250];
-    let mut buffered_stdin = BufReader::new(stdin());
 
-    let mut reader = EventStream::new();
+    let mut reader = EventStream::new();        
     let mut cursor_position: u16 = 0;
     let mut input_line: Vec<char> = Vec::new();
 
@@ -61,40 +93,58 @@ async fn main()-> Result<(), Box<dyn Error>> {
         select! {
             //await inputs from tcp stream
             _ = async{stream.peek(&mut [0u8,1]).await }.fuse() =>{
-                println!("reading from stream");
-                let n = stream.read(&mut buffer).await?;
-                // stdout().write_all(&buffer[..n])?;
+                stdout().execute(Clear(crossterm::terminal::ClearType::CurrentLine));
+                let n = match stream.read(&mut buffer).await{
+                    Ok(n) => n,
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        break Err(e);
+                    }
+                };
+                print_with_style(buffer[..n].to_vec(), '>', Color::Cyan);
             }
-            //await inputs from stdin
-            // _ = async{buffered_stdin.read_line(&mut input_string).await}.fuse() =>{
-            //     println!("writing to stream");
-            //     stream.write(&input_string.as_bytes()).await?;
-            // }
+            // catch any keyboard activity 
             maybe_event = event=> {
                 match maybe_event{
                     Some(Ok(event)) => {
                         match event {
+                            // Event: Any character key is pressed
+                            // the character is added to the input vector at cursor position
                             Event::Key(KeyEvent{code: KeyCode::Char(c), modifiers: KeyModifiers::NONE, kind: crossterm::event::KeyEventKind::Press, ..}) | 
                             Event::Key(KeyEvent{code: KeyCode::Char(c), modifiers: KeyModifiers::SHIFT, kind: crossterm::event::KeyEventKind::Press, ..})=> {
                                 input_line.insert(cursor_position as usize, c);
                                 cursor_position += 1;
                             }
+                            // Event: Enter key is pressed
+                            // the input vector is saved and sent to the server
+                            // the input line is reset
                             Event::Key(KeyEvent{code: KeyCode::Enter, modifiers: KeyModifiers::NONE, kind: crossterm::event::KeyEventKind::Press, ..}) => {
-                                println!("writing to stream");
-                                stream.write(&input_line.iter().map(|c| *c as u8).collect::<Vec<_>>()).await?;
+                                //save input to history
+                                input_line.push('\n');
+                                let input_u8_vector = &input_line.iter().map(|c| *c as u8).collect::<Vec<_>>();
+                                print_with_style(input_u8_vector.to_vec(), '<', Color::Green);
+                                stream.write(&input_u8_vector).await?;
+                                input_line.clear();
+                                cursor_position = 0;
                             }
+                            // Event: Backspace key is pressed
+                            // the character at cursor position is removed from the input vector
                             Event::Key(KeyEvent{code: KeyCode::Backspace, modifiers: KeyModifiers::NONE, kind: crossterm::event::KeyEventKind::Press, ..}) => {
                                 if input_line.len() > 0 && cursor_position > 0{
                                     input_line.remove(cursor_position as usize - 1);
                                     cursor_position -= 1;
                                 }
                             }
+                            //Event: Left arrow key is pressed
+                            // the cursor position is moved left
                             Event::Key(KeyEvent{code: KeyCode::Left, modifiers: KeyModifiers::NONE, kind: crossterm::event::KeyEventKind::Press, ..}) => {
                                 // cursor::MoveLeft(1);
                                 if cursor_position > 0{
                                     cursor_position -= 1;
                                 }
                             }
+                            // Event: Right arrow key is pressed
+                            // the cursor position is moved right
                             Event::Key(KeyEvent{code: KeyCode::Right, modifiers: KeyModifiers::NONE, kind: crossterm::event::KeyEventKind::Press, ..}) => {
                                 // cursor::MoveRight(1);
                                 if cursor_position < input_line.len() as u16{
@@ -105,14 +155,31 @@ async fn main()-> Result<(), Box<dyn Error>> {
                         }
                     }
                     Some(Err(e)) => println!("Error:{:?}", e),
-                    None => break Ok(()),
+                    None =>  {},
                 }
             }
-        }
-        // print_vector(&input_line);
-        write_vec_to_console(&input_line);
-        move_cursor(cursor_position)?;
-        buffer = [0u8; 250];    //clear buffer
-        input_string.clear();
+        }        
+        move_cursor(cursor_position);          //move terminal cursor to the new cursor position
+        write_vec_to_console(&input_line);      //display current input line
+        buffer = [0u8; 250];                    //clear buffer            
     }
+}
+
+#[async_std::main]
+async fn main()-> Result<(), Box<dyn Error>> {
+    stdout()
+        .execute(cursor::EnableBlinking)?
+        .execute(DisableFocusChange)?
+        .execute(Clear(crossterm::terminal::ClearType::All))?;
+
+    let cli = Cli::parse();
+    println!("Listening on: {}", cli.listen_address);
+    match main_loop(cli.listen_address).await{
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error: {:?}", e);
+            main_loop(cli.listen_address).await?;
+        }
+    }
+    Ok(())   
 }
