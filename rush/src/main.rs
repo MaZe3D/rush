4565ffa6f9f622bc010c8a06d6364e3ad36c287a
MaZe <45102464+MaZe3D@users.noreply.github.com>
2023-05-12 15:25:14 +0200

    add allocator

--- rush/src/main.rs
+++ rush/src/main.rs
@@ -7,6 +7,10 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+extern crate alloc;
+
+
+
 use embassy_executor::Executor;
 use embassy_time::{Duration, Timer};
 use esp32s3_hal::{
@@ -15,7 +19,7 @@ use esp32s3_hal::{
     peripherals::{self, Peripherals},
     prelude::*,
     timer::TimerGroup,
-    Rtc, IO, gpio::Output,
+    Rtc, IO, gpio::Output, soc,
 };
 use esp_backtrace as _;
 use esp_println::println;
@@ -29,17 +33,39 @@ mod rush_gpio_manager;
 
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+fn init_psram_heap() {
+    unsafe {
+        ALLOCATOR.init(
+            soc::psram::PSRAM_VADDR_START as *mut u8,
+            soc::psram::PSRAM_BYTES,
+        );
+    }
+}
+
 #[entry]
 fn main() -> ! {
     println!("Init!");
     let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
+    soc::psram::init_psram(peripherals.PSRAM);
+    init_psram_heap();
+    let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
     let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let timer_group1 = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
     let mut wdt1 = timer_group1.wdt;
 
     command_evaluator::evaluate_command("read gpio.1");
@@ -53,7 +79,7 @@ fn main() -> ! {
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     let a = io.pins.gpio47.into_floating_input();
-    
+
     let a = a.into_pull_up_input();
     let mut b = a.is_high().unwrap();
     println!("a: {}", b);
--- rush/src/rush_gpio_manager.rs
+++ rush/src/rush_gpio_manager.rs
@@ -27,16 +27,8 @@ pub fn read_pin(pins: esp32s3_hal::soc::gpio::Pins, gpio_id: u8) -> bool {
     let mut pin =
         match gpio_id {
         0 => pins.gpio0,
-        1 => pins.gpio1.into_pull_up_input(),
-        2 => pins.gpio2,
-        3 => pins.gpio3,
-        4 => pins.gpio4,
-        5 => pins.gpio5,
-        6 => pins.gpio6,
-        7 => pins.gpio7,
+        _ => pins.gpio0,
+    };
 
-        _ => println!("Invalid gpio id"),
-    }
-
-    return result;
+    return false;
 }
