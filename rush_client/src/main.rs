10955f9ea0e1f89820839b6143e1c54338ba18b1
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2023-05-15 14:15:31 +0200

    client: simplify, ...

--- rush_client/src/main.rs
+++ rush_client/src/main.rs
@@ -1,15 +1,15 @@
-use std::error::Error;
-use std::io::stdout;
-
 use async_std::io::prelude::*;
 use async_std::net::{SocketAddr, TcpStream};
-use futures::{select, FutureExt, StreamExt};
-
 use clap::Parser;
-use crossterm::event::{DisableFocusChange, Event, EventStream, KeyCode, KeyEvent, KeyModifiers, KeyEventKind};
+use crossterm::event::{
+    DisableFocusChange, Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers,
+};
 use crossterm::style::{Color, Print, ResetColor, SetForegroundColor};
 use crossterm::terminal::{Clear, DisableLineWrap, EnableLineWrap};
 use crossterm::{cursor, execute};
+use futures::{select, FutureExt, StreamExt};
+use std::error::Error;
+use std::io::stdout;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -22,46 +22,17 @@ fn print_error(e: impl Error) {
     println!();
 }
 
-//move the cursor to the bottom line of the console, at the specified column
-fn move_cursor(n: u16) -> std::io::Result<()> {
-    let bottom_row = match crossterm::terminal::size() {
-        Ok(n) => n.0,
-        Err(e) => {
-            print_error(e);
-            0
-        }
-    };
-    match execute! {
-        stdout(),
-        cursor::MoveTo(n, bottom_row-1),
-    } {
-        Ok(_) => {}
-        Err(e) => print_error(e),
-    };
-    Ok(())
-}
-
 //print a vector of chars to the bottom line of the console
-fn write_vec_to_console(vec: &Vec<char>) {
-    let mut stdout = stdout();
-    match execute! {
-        stdout,
+fn write_vec_to_console(vec: &Vec<char>) -> Result<(), std::io::Error> {
+    execute! {
+        stdout(),
         cursor::SavePosition,
         cursor::MoveToColumn(0),
         Clear(crossterm::terminal::ClearType::CurrentLine),
-    } {
-        Ok(_) => {}
-        Err(e) => print_error(e),
-    };
-    print!(
-        "{}",
-        vec.iter()
-            .fold(String::new(), |acc, &num| acc + &num.to_string())
-    );
-    match execute! {stdout, cursor::RestorePosition} {
-        Ok(_) => {}
-        Err(e) => print_error(e),
-    };
+        Print(vec.iter().fold(String::new(), |acc, &num| acc + &num.to_string())),
+        cursor::RestorePosition,
+    }?;
+    Ok(())
 }
 
 // print a vector to the bottom line of the console with specified start character and color
@@ -130,11 +101,14 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
             }
 
             // catch any keyboard activity
-            maybe_event = reader.next().fuse() => match maybe_event {
-                Some(Ok(Event::Key(KeyEvent { code, modifiers, kind: KeyEventKind::Press, state: _ }))) => {
+            event = reader.next().fuse() => {
+                if let Some(Err(e)) = event {
+                    break Err(e);
+                }
+                if let Some(Ok(Event::Key(KeyEvent { code, modifiers, kind: KeyEventKind::Press, state: _ }))) = event {
                     match (code, modifiers) {
 
-                        // Event: Any character key is pressed
+                        // event: any character key is pressed
                         // the character is added to the input vector at cursor position
                         (KeyCode::Char(c), KeyModifiers::NONE) |
                         (KeyCode::Char(c), KeyModifiers::SHIFT) => {
@@ -142,37 +116,32 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                             cursor_position += 1;
                         }
 
-                        // Event: Enter key is pressed
+                        // event: enter key is pressed
                         // the input vector is saved and sent to the server
                         // the input line is reset
                         (KeyCode::Enter, KeyModifiers::NONE) |
                         (KeyCode::Char('m'), KeyModifiers::CONTROL) |
                         (KeyCode::Char('j'), KeyModifiers::CONTROL) => {
-                            if history.len() >= 2{
-                                if input_line != history[1]{
+                            if history.len() >= 2 {
+                                if input_line != history[1] {
                                     history.insert(1, input_line.clone());
                                 }
                             }
-                            else{
+                            else {
                                 history.insert(1, input_line.clone());
                             }
                             input_line.push('\n');
                             let input_u8_vector = &input_line.iter().map(|c| *c as u8).collect::<Vec<_>>();
-                            match execute!{stdout(), EnableLineWrap}{
-                                Ok(_) => {}
-                                Err(e) => print_error(e),
-                            };
+                            execute!{stdout(), EnableLineWrap}?;
                             print_with_style(input_u8_vector.to_vec(), "OUT ", Color::Green);
-                            match execute!{stdout(), DisableLineWrap}{
-                                Ok(_) => {}
-                                Err(e) => print_error(e),
-                            };
+                            execute!{stdout(), DisableLineWrap}?;
                             stream.write(&input_u8_vector).await?;
                             input_line.clear();
                             history_position = 0;
                             cursor_position = 0;
                         }
-                        // Event: Backspace key is pressed
+
+                        // event: backspace key is pressed
                         // the character at cursor position is removed from the input vector
                         (KeyCode::Backspace, KeyModifiers::NONE) |
                         (KeyCode::Char('h'), KeyModifiers::CONTROL) => {
@@ -182,7 +151,7 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                             }
                         }
 
-                        //Event: Left arrow key is pressed
+                        //event: left arrow key is pressed
                         // the cursor position is moved left
                         (KeyCode::Left, KeyModifiers::NONE) => {
                             if cursor_position > 0{
@@ -190,7 +159,7 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                             }
                         }
 
-                        // Event: Right arrow key is pressed
+                        // event: right arrow key is pressed
                         // the cursor position is moved right
                         (KeyCode::Right, KeyModifiers::NONE) => {
                             if cursor_position < input_line.len() as u16{
@@ -198,7 +167,7 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                             }
                         }
 
-                        // Event: Up arrow key is pressed
+                        // event: up arrow key is pressed
                         // the input vector is replaced with the previous input vector in the history vector
                         (KeyCode::Up, KeyModifiers::NONE) |
                         (KeyCode::Char('p'), KeyModifiers::CONTROL) => {
@@ -209,7 +178,7 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                             }
                         }
 
-                        // Event: Down arrow key is pressed
+                        // event: down arrow key is pressed
                         // the input vector is replaced with the next input vector in the history vector
                         (KeyCode::Down, KeyModifiers::NONE) |
                         (KeyCode::Char('n'), KeyModifiers::CONTROL) => {
@@ -219,7 +188,8 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                                 cursor_position = input_line.len() as u16;
                             }
                         }
-                        // Event: Delete key is pressed
+
+                        // event: delete key is pressed
                         // the character at cursor position is removed from the input vector
                         (KeyCode::Delete, KeyModifiers::NONE) => {
                             if input_line.len() > 0 && cursor_position < input_line.len() as u16{
@@ -230,8 +200,6 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
                         _ => (),
                     }
                 }
-                Some(Err(e)) => print_error(e),
-                _ =>  {},
             }
         }
 
@@ -240,15 +208,13 @@ async fn main_loop(address: SocketAddr) -> Result<(), std::io::Error> {
         }
 
         // move terminal cursor to the new cursor position
-        match move_cursor(cursor_position) {
-            Ok(_) => {}
-            Err(e) => {
-                print_error(e);
-            }
-        }
+        execute! {
+            stdout(),
+            cursor::MoveTo(cursor_position, crossterm::terminal::size()?.0 - 1),
+        }?;
 
         // display current input line
-        write_vec_to_console(&input_line);
+        write_vec_to_console(&input_line)?;
     }
 }
 
